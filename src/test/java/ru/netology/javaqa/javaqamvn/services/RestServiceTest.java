20e9234f5de306ccaa0d225bc809706e876c56bb
MurashovS <imurasov0@gmail.com>
2023-12-18 22:05:10 +0300

    first commit

---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/RestServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RestServiceTest {
+    @Test
+    public void RestServiceTest() {
+
+
+        RestService service = new RestService();
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    public void WorkRestServiceTest() {
+
+
+        RestService service = new RestService();
+        int restexpected = 2;
+        int restactual = service.calculate(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(restexpected, restactual);
+    }
+}
+
+
+
